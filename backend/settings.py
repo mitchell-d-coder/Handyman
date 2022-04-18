d736038e74be152f749ae0be2b01c2efabb03ce6
proflamyt <53262578+proflamyt@users.noreply.github.com>
2022-04-18 23:20:33 +0100

    olamide

--- backend/settings.py
+++ backend/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'website',
+    'rest_framework',
+    'authentication'
+
 ]
 
 MIDDLEWARE = [
@@ -128,3 +132,4 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'authentication.User'
--- website/urls.py
+++ website/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.home , name="home"),
     path("gallery", views.gallery , name="gallery"),
-    path("blog", views.blog , name="blog"),
+    path("blog", views.blogs , name="blog"),
     path("about", views.about , name="about"),
     path("contact", views.contact , name="contact"),
     path("menu", views.menu , name="menu"),
